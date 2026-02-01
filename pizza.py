81919f23ca9bc2d8f996d223ce23d285157ce08c
KaaviyasriVarshini <kaaviyasrivarshinir@gmail.com>
2026-02-01 17:14:51 +0530

    py notes

---
+++ pizza.py
@@ -0,0 +1,17 @@
+#Write a Python program for a pizza delivery system that:
+	#•	Asks the user for the pizza size (S, M, or L)
+	#•	Asks whether the user wants pepperoni (Y or N)
+	#•	Asks whether the user wants extra cheese (Y or N)
+	#•	Calculates the final bill based on the following rules:
+	#•	Small pizza costs $15, medium $20, large $25
+	#•	Pepperoni costs $2 for small pizzas and $3 for medium and large pizzas
+	#•	Extra cheese costs $1 for any size
+	#•	Displays an error message if an invalid pizza size is selected
+	#•	Prints the final bill only if a valid size is chosen
+
+
+pizza_size = input(str("Enter the pizza size (S, M, or L)"))
+pepperoni = input("Do you want pepperoni? (Y or N)")
+extra_cheese = input("Do you want extra cheese? (Y or N)")
+if pizza_size == str(S):
+    if 
